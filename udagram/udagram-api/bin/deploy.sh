8119e99ffe68928d4f0d6f7c18102d436829f6fa
Luong Trung <trunghlbuh@gmail.com>
2023-09-14 03:51:01 +0700

    update deploy

---
+++ udagram/udagram-api/bin/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+eb init udagram-api --platform node.js --region us-east-1
+
+eb use udagram-api-dev
+
+eb setenv AWS_BUCKET=AWS_BUCKET POSTGRES_HOST=POSTGRES_HOST DB_PORT=DB_PORT POSTGRES_DB=POSTGRES_DB POSTGRES_USERNAME=POSTGRES_USERNAME POSTGRES_PASSWORD=POSTGRES_PASSWORD
+
+eb deploy udagram-api-dev
